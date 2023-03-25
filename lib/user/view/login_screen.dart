a63ba778ff402560b2e0745293f7cdee6ae6771a
jincrates <94jingyu@gmail.com>
2023-03-25 11:59:23 +0900

    feat: 로그인 텍스트필드 키보드 영역 처리

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -9,47 +9,58 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      child: SafeArea(
-        top: true,
-        bottom: false,
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const _Title(),
-            const _SubTitle(),
-            Image.asset(
-                'asset/img/misc/logo.png',
-              width: MediaQuery.of(context).size.width /3 * 2,
+      // 키보드가 올라와도 위젯이 가려지지 않도록 스크롤 처리
+      child: SingleChildScrollView(
+        // 드래그하는 순간에 키보드가 사라지도록 사용성을 높일 수 있다.
+        keyboardDismissBehavior: ScrollViewKeyboardDismissBehavior.onDrag,
+        child: SafeArea(
+          top: true,
+          bottom: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                _Title(),
+                const SizedBox(height: 16.0),
+                _SubTitle(),
+                Image.asset(
+                    'asset/img/misc/logo.png',
+                  width: MediaQuery.of(context).size.width /3 * 2,
+                ),
+                // cmd + shift + k: 키보드 타이핑 올라옴
+                CustomTextFormField(
+                  hintText: '이메일을 입력해주세요.',
+                  onChanged: (String value) {},
+                ),
+                const SizedBox(height: 16.0),
+                CustomTextFormField(
+                  hintText: '비밀번호를 입력해주세요.',
+                  onChanged: (String value) {},
+                  obscureText: true,
+                ),
+                const SizedBox(height: 16.0),
+                ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: PRIMARY_COLOR,
+                  ),
+                  child: const Text(
+                    '로그인',
+                  ),
+                ),
+                TextButton(
+                  onPressed: () {},
+                  style: TextButton.styleFrom(
+                    foregroundColor: Colors.black,
+                  ),
+                  child: const Text(
+                    '회원가입',
+                  ),
+                ),
+              ],
             ),
-            // cmd + shift + k: 키보드 타이핑 올라옴
-            CustomTextFormField(
-              hintText: '이메일을 입력해주세요.',
-              onChanged: (String value) {},
-            ),
-            CustomTextFormField(
-              hintText: '비밀번호를 입력해주세요.',
-              onChanged: (String value) {},
-              obscureText: true,
-            ),
-            ElevatedButton(
-              onPressed: () {},
-              style: ElevatedButton.styleFrom(
-                backgroundColor: PRIMARY_COLOR,
-              ),
-              child: const Text(
-                '로그인',
-              ),
-            ),
-            TextButton(
-              onPressed: () {},
-              style: TextButton.styleFrom(
-                foregroundColor: Colors.black,
-              ),
-              child: const Text(
-                '회원가입',
-              ),
-            ),
-          ],
+          ),
         ),
       ),
     );

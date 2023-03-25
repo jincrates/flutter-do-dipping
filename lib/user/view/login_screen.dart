e69fcb6046c5d90dba9c6a3957f204cdb5384e16
jincrates <94jingyu@gmail.com>
2023-03-25 11:50:23 +0900

    feat: Login 버튼 추가

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -1,3 +1,4 @@
+import 'package:actual/common/const/colors.dart';
 import 'package:flutter/material.dart';
 import '../../common/const/component/custom_text_form_field.dart';
 import '../../common/layout/default_layout.dart';
@@ -14,7 +15,12 @@ class LoginScreen extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            _Title(),
+            const _Title(),
+            const _SubTitle(),
+            Image.asset(
+                'asset/img/misc/logo.png',
+              width: MediaQuery.of(context).size.width /3 * 2,
+            ),
             // cmd + shift + k: 키보드 타이핑 올라옴
             CustomTextFormField(
               hintText: '이메일을 입력해주세요.',
@@ -24,6 +30,24 @@ class LoginScreen extends StatelessWidget {
               hintText: '비밀번호를 입력해주세요.',
               onChanged: (String value) {},
               obscureText: true,
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              style: ElevatedButton.styleFrom(
+                backgroundColor: PRIMARY_COLOR,
+              ),
+              child: const Text(
+                '로그인',
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.black,
+              ),
+              child: const Text(
+                '회원가입',
+              ),
             ),
           ],
         ),
@@ -43,6 +67,21 @@ class _Title extends StatelessWidget {
         fontSize: 34,
         fontWeight: FontWeight.w500,
         color: Colors.black,
+      ),
+    );
+  }
+}
+
+class _SubTitle extends StatelessWidget {
+  const _SubTitle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      '이메일과 비밀번호를 입력해서 로그인 해주세요!\n오늘도 성공적인 주문이 되길 :)',
+      style: TextStyle(
+        fontSize: 16,
+        color: BODY_TEXT_COLOR,
       ),
     );
   }

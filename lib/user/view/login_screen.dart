279901824745198816acb213e6b1fa036d23b625
jincrates <94jingyu@gmail.com>
2023-03-25 11:39:24 +0900

    feat: title 작업 추가

--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -8,21 +8,43 @@ class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center, //가운데 정렬
-        children: const [
-          // cmd + shift + k: 키보드 타이핑 올라옴
-          CustomTextFormField(
-            hintText: '이메일을 입력해주세요.',
-            onChanged: (String value) {},
-          ),
-          CustomTextFormField(
-            hintText: '비밀번호를 입력해주세요.',
-            onChanged: (String value) {},
-            obscureText: true,
-          ),
-        ],
+      child: SafeArea(
+        top: true,
+        bottom: false,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            _Title(),
+            // cmd + shift + k: 키보드 타이핑 올라옴
+            CustomTextFormField(
+              hintText: '이메일을 입력해주세요.',
+              onChanged: (String value) {},
+            ),
+            CustomTextFormField(
+              hintText: '비밀번호를 입력해주세요.',
+              onChanged: (String value) {},
+              obscureText: true,
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
+class _Title extends StatelessWidget {
+  const _Title({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text(
+      '환영합니다!',
+      style: TextStyle(
+        fontSize: 34,
+        fontWeight: FontWeight.w500,
+        color: Colors.black,
+      ),
+    );
+  }
+}
+

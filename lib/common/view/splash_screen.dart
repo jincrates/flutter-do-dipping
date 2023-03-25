fc5694c85775d123ad1a82fbdf3f313eabd82588
jincrates <94jingyu@gmail.com>
2023-03-25 17:17:20 +0900

    feat: splash 페이지 추가 및 토큰 검증 처리

---
+++ lib/common/view/splash_screen.dart
@@ -0,0 +1,75 @@
+import 'package:actual/common/const/data.dart';
+import 'package:actual/common/layout/default_layout.dart';
+import 'package:actual/common/view/root_tab.dart';
+import 'package:actual/user/view/login_screen.dart';
+import 'package:flutter/material.dart';
+
+import '../const/colors.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+
+    // initState에서는 async를 사용할 수 없다.
+    // deleteToken();
+    checkToken();
+  }
+
+  void checkToken() async {
+    final refreshToken = await storage.read(key: REFRESH_TOKEN_KEY);
+    final accessToken = await storage.read(key: ACCESS_TOKEN_KEY);
+
+    // TODO: 토큰이 유효한지도 체크해야 한다.
+    if (refreshToken == null || accessToken == null) {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(
+            builder: (_) => LoginScreen(),
+          ),
+          (route) => false,
+      );
+    } else {
+      Navigator.of(context).pushAndRemoveUntil(
+        MaterialPageRoute(
+          builder: (_) => RootTab(),
+        ),
+            (route) => false,
+      );
+    }
+  }
+
+  void deleteToken() async {
+    await storage.deleteAll();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultLayout(
+        backgroundColor: PRIMARY_COLOR,
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                'asset/img/logo/logo.png',
+                width: MediaQuery.of(context).size.width /2,
+              ),
+              const SizedBox(height: 16.0),
+              CircularProgressIndicator(
+                color: Colors.white,
+              ),
+            ],
+          ),
+        )
+    );
+  }
+}
--- lib/user/view/login_screen.dart
+++ lib/user/view/login_screen.dart
@@ -2,13 +2,23 @@ import 'dart:convert';
 import 'dart:io';
 
 import 'package:actual/common/const/colors.dart';
+import 'package:actual/common/const/data.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import '../../common/const/component/custom_text_form_field.dart';
+import '../../common/component/custom_text_form_field.dart';
 import '../../common/layout/default_layout.dart';
+import '../../common/view/root_tab.dart';
 
-class LoginScreen extends StatelessWidget {
+class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  String username = '';
+  String password = '';
 
   @override
   Widget build(BuildContext context) {
@@ -43,18 +53,23 @@ class LoginScreen extends StatelessWidget {
                 // cmd + shift + k: 키보드 타이핑 올라옴
                 CustomTextFormField(
                   hintText: '이메일을 입력해주세요.',
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    username = value;
+                  },
                 ),
                 const SizedBox(height: 16.0),
                 CustomTextFormField(
                   hintText: '비밀번호를 입력해주세요.',
-                  onChanged: (String value) {},
+                  onChanged: (String value) {
+                    password = value;
+                  },
                   obscureText: true,
                 ),
                 const SizedBox(height: 16.0),
                 ElevatedButton(
                   onPressed: () async {
                     //ID:PASSWORD
+                    // final rawString = '$username:$password';
                     final rawString = 'test@codefactory.ai:testtest';
 
                     //base64 인코딩하는 법
@@ -69,7 +84,20 @@ class LoginScreen extends StatelessWidget {
                         },
                       ),
                     );
-                    print(resp.data);
+
+                    final refreshToken = resp.data["refreshToken"];
+                    final accessToken = resp.data["accessToken"];
+
+                    // 보안 저장소에 데이터 저장
+                    await storage.write(key: REFRESH_TOKEN_KEY, value: refreshToken);
+                    await storage.write(key: ACCESS_TOKEN_KEY, value: accessToken);
+
+                    // 로그인 처리 후 페이지 이동
+                    Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (_) => RootTab(),
+                      ),
+                    );
                   },
                   style: ElevatedButton.styleFrom(
                     backgroundColor: PRIMARY_COLOR,

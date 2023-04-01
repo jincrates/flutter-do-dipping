fc2989e2d90cdacac5980f30027a010b60538233
jincrates <94jingyu@gmail.com>
2023-04-01 11:12:21 +0900

    feat: onResponse 구현

--- lib/common/dio/dio.dart
+++ lib/common/dio/dio.dart
@@ -48,6 +48,12 @@ class CustomInterceptor extends Interceptor {
   }
 
   // 2) 응답을 받을 때
+  @override
+  void onResponse(Response response, ResponseInterceptorHandler handler) {
+    print('[RES] [${response.requestOptions.method}] ${response.requestOptions.uri}');
+
+    return super.onResponse(response, handler);
+  }
 
   // 3) 에러가 났을 때
   @override
